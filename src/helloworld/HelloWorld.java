480b086d216fc98f9b8ad944f5c91c0153064c1b
Josh <josh.farrell@pcc.edu>
2020-04-20 21:07:13 -0700

    Added HelloWorld Class

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -1,4 +1,12 @@
 package helloworld;
 
+/**
+ * Author: Josh Farrell
+ * Date: 4/20/20
+ */
+
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
